ab1d99cc43052e7b2ce6de2f046f1a5b75de568f
aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2025-11-20 23:33:41 +0100

    npc preview scene - draft

---
+++ Assets/Scripts/NpcPreviewGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcPreviewGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private NpcData data;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
