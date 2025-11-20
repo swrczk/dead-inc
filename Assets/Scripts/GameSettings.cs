62c57a4b98f3ea87e177ee1bc0bb59a6c22ab18f
aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2025-11-20 20:13:42 +0100

    market scene

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -3,18 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
+[CreateAssetMenu(fileName = "GameSettings", menuName = "GameSettings")]
 public class GameSettings : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int Time;
 }
