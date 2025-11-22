1a5e8d256316d12ea963fc71871964f7de728f0f
aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2025-11-22 10:25:07 +0100

    fix

--- Assets/Scripts/CutscenePanel.cs
+++ Assets/Scripts/CutscenePanel.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CutscenePanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private VideoPlayer videoPlayer;
 
-    // Update is called once per frame
-    void Update()
+    public void Play(VideoClip clip)
     {
         
     }
