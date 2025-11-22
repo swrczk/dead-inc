8b5cccf96bebbab080310fc007a3c5b5a2580766
aleksandra.swierczek <aleksandra.swierczek@tensquaregames.com>
2025-11-22 11:52:30 +0100

    close scene on start view

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,11 +10,23 @@ public class StartScreen : MonoBehaviour
 { 
     [SerializeField]
     private Button button; 
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private GameObject screenToHide;
     private void Start()
     {
         button.onClick.AddListener(() => StartGame());
+        PlayAndHide();
     }
-    
+
+    private async void PlayAndHide()
+    {
+        await UniTask.WaitUntil(()=>videoPlayer.isPlaying);
+        await UniTask.WaitUntil(()=>!videoPlayer.isPlaying);
+        screenToHide.SetActive(false);
+    }
+
     private void StartGame()
     {
         SceneManager.LoadScene("Market");
